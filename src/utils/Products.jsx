80e628af80c3c1ad65c6711caf0fb93041cceb41
Maxi Alzugaray <maxialz99@hotmail.com>
2023-04-30 23:40:09 -0300

    actualizo precios

--- src/utils/Products.jsx
+++ src/utils/Products.jsx
@@ -8,13 +8,13 @@ const productsInMenu = {
     { name: "gin tonic gordon's", size: "grande", price: 1500 },
   ],
   beers: [
-    { name: "stella artois", size: "710 cc", price: 1000 },
+    { name: "stella artois", size: "710 cc", price: 1300 },
     { name: "lata budweiser", size: "", price: 600 },
   ],
   whithoutAlcoholDrinks: [
     { name: "agua mineral", size: "chica", price: 400 },
     { name: "speed", size: "", price: 500 },
-    { name: "vaso de gaseosa", size: "chico", price: 400 },
+    { name: "vaso de gaseosa", size: "chico", price: 500 },
   ],
   sales: [
     { amount: 2, name: "Fernet branca + coca", size: "chico", price: 1700 },
@@ -22,7 +22,9 @@ const productsInMenu = {
     { amount: 2, name: "smirnoff + speed", size: "chico", price: 1900 },
     { amount: 2, name: "smirnoff + speed", size: "grande", price: 2600 },
     { amount: 2, name: "lata budweiser", size: "", price: 1000 },
-    { amount: 3, name: "speed", size: "", price: 1000 },
+    { amount: 3, name: "Stella astois", size: "", price: 2000 },
+    { amount: 2, name: "speed", size: "", price: 1000 },
+    
   ],
 };
 
